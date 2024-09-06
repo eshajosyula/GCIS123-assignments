42a2314503007fef807075fc32827669144a48b8
eshajosyula <gayatri.josyula@gmail.com>
2024-09-06 10:38:11 -0400

    add happy_birthday.py

---
+++ happy_birthday.py
@@ -0,0 +1,15 @@
+def happy_birthday():
+    name = input("your name: ")
+    month = input("birth month: ")
+    day = input("day of your birth: ")
+    year = input("birth year: ")
+
+    print(name, ", your birthday is on", month, day, ",", year)
+
+
+def happy_birthday2(name, month, day, year):
+    print(name, ", your birthday is on", month, day, ",", year)
+
+
+happy_birthday2("esha","october","22","2006")
+
