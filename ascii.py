7584b0f6e61223fc55835953ec248a9558af8a6c
eshajosyula <gayatri.josyula@gmail.com>
2024-09-06 11:48:14 -0400

    convert to ascii!!

---
+++ ascii.py
@@ -0,0 +1,11 @@
+def convert_to_ascii():
+    char = input("enter a single character : ")
+    chartoascii = ord(char)
+    print(chartoascii)
+
+def main():
+    convert_to_ascii()
+    convert_to_ascii()
+    convert_to_ascii()
+
+main()
